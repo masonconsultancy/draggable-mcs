e958f82b1505bd501d68f2b3017b7e351968ebd3
Gary John Mason <gary.john.mason@gmail.com>
2023-03-05 11:10:38 +0000

    Changed spacing to 2 in js files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,249 +1,249 @@
 module.exports = function (grunt) {
-    // From TWBS
-    RegExp.quote = function (string) {
-        return string.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&');
-    };
+  // From TWBS
+  RegExp.quote = function (string) {
+    return string.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&');
+  };
 
-    // Project configuration.
-    grunt.initConfig({
+  // Project configuration.
+  grunt.initConfig({
 
-        // Metadata.
-        pkg: grunt.file.readJSON('package.json'),
-        banner: '/*!\n' +
-            ' * draggable-mcs v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
-            ' *\n' +
-            ' * Copyright 2023-<%= grunt.template.today(\'yyyy\') %> Mason Consultancy Ltd\n' +
-            ' * Licensed under <%= pkg.license %> (https://github.com/masonconsultancy/draggable-mcs/blob/master/LICENSE)\n' +
-            ' */\n',
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*!\n' +
+      ' * draggable-mcs v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+      ' *\n' +
+      ' * Copyright 2023-<%= grunt.template.today(\'yyyy\') %> Mason Consultancy Ltd\n' +
+      ' * Licensed under <%= pkg.license %> (https://github.com/masonconsultancy/draggable-mcs/blob/master/LICENSE)\n' +
+      ' */\n',
 
-        // Task configuration.
+    // Task configuration.
 
-        clean: {
-            css: 'dist/css',
-            js: 'dist/js'
+    clean: {
+      css: 'dist/css',
+      js: 'dist/js'
+    },
+
+    eslint: {
+      options: {
+        configFile: 'js/.eslintrc.json'
+      },
+      gruntfile: {
+        options: {
+          'envs': [
+            'node'
+          ]
         },
+        src: 'Gruntfile.js'
+      },
+      main: {
+        src: 'js/*.js'
+      }
+    },
 
-        eslint: {
-            options: {
-                configFile: 'js/.eslintrc.json'
-            },
-            gruntfile: {
-                options: {
-                    'envs': [
-                        'node'
-                    ]
-                },
-                src: 'Gruntfile.js'
-            },
-            main: {
-                src: 'js/*.js'
-            }
-        },
-
-        concat: {
-            options: {
-                stripBanners: true,
-                sourceMap: true
-            },
-            main: {
-                src: 'js/<%= pkg.name %>.js',
-                dest: 'dist/js/<%= pkg.name %>.js',
-                options: {
-                    banner: '<%= banner %>\n' + grunt.file.read('js/umd-intro.js'),
-                    footer: grunt.file.read('js/umd-outro.js')
-                }
-            }
-        },
-
-        uglify: {
-            options: {
-                banner: '<%= banner %>',
-                output: {
-                    ascii_only: true
-                },
-                preserveComments: function (node, comment) {
-                    return /^!|@preserve|@license|@cc_on/i.test(comment.value);
-                }
-            },
-            main: {
-                src: '<%= concat.main.dest %>',
-                dest: 'dist/js/<%= pkg.name %>.min.js',
-                options: {
-                    sourceMap: true,
-                    sourceMapIncludeSources: true,
-                    sourceMapIn: 'dist/js/<%= pkg.name %>.js.map'
-                }
-            }
-        },
-
-        less: {
-            options: {
-                strictMath: true,
-                sourceMap: true,
-                outputSourceFiles: true,
-                sourceMapURL: '<%= pkg.name %>.css.map',
-                sourceMapFilename: '<%= less.css.dest %>.map'
-            },
-            css: {
-                src: 'less/draggable-mcs.less',
-                dest: 'dist/css/<%= pkg.name %>.css'
-            }
-        },
-
-        usebanner: {
-            css: {
-                options: {
-                    banner: '<%= banner %>'
-                },
-                src: '<%= less.css.dest %>'
-            }
-        },
-
-        copy: {
-            docs: {
-                expand: true,
-                cwd: 'dist/',
-                src: [
-                    '**/*'
-                ],
-                dest: 'docs/docs/dist/'
-            }
-        },
-
-        cssmin: {
-            options: {
-                compatibility: 'ie8',
-                keepSpecialComments: '*',
-                advanced: false
-            },
-            css: {
-                src: '<%= less.css.dest %>',
-                dest: 'dist/css/<%= pkg.name %>.min.css'
-            }
-        },
-
-        csslint: {
-            options: {
-                'adjoining-classes': false,
-                'box-sizing': false,
-                'box-model': false,
-                'compatible-vendor-prefixes': false,
-                'floats': false,
-                'font-sizes': false,
-                'gradients': false,
-                'important': false,
-                'known-properties': false,
-                'outline-none': false,
-                'qualified-headings': false,
-                'regex-selectors': false,
-                'shorthand': false,
-                'text-indent': false,
-                'unique-headings': false,
-                'universal-selector': false,
-                'unqualified-attributes': false,
-                'overqualified-elements': false
-            },
-            css: {
-                src: '<%= less.css.dest %>'
-            }
-        },
-
-        version: {
-            js: {
-                options: {
-                    prefix: 'draggable-mcs.VERSION = \''
-                },
-                src: [
-                    'js/<%= pkg.name %>.js'
-                ]
-            },
-            default: {
-                options: {
-                    prefix: '[\'"]?version[\'"]?:[ "\']*'
-                },
-                src: [
-                    'package.json'
-                ]
-            }
-        },
-
-        postcss: {
-            options: {
-                map: true,
-                processors: [
-                    require('autoprefixer')()
-                ]
-            },
-            css: {
-                src: '<%= less.css.dest %>'
-            }
-        },
-
-        compress: {
-            zip: {
-                options: {
-                    archive: 'draggable-mcs-<%= pkg.version %>.zip',
-                    mode: 'zip'
-                },
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'dist/',
-                        src: '**',
-                        dest: 'draggable-mcs-<%= pkg.version %>/'
-                    }, {
-                        src: ['package.json'],
-                        dest: 'draggable-mcs-<%= pkg.version %>/'
-                    }
-                ]
-            }
-        },
-
-        watch: {
-            gruntfile: {
-                files: '<%= eslint.gruntfile.src %>',
-                tasks: 'eslint:gruntfile'
-            },
-            js: {
-                files: ['<%= eslint.main.src %>'],
-                tasks: 'build-js'
-            },
-            less: {
-                files: 'less/*.less',
-                tasks: 'build-css'
-            }
+    concat: {
+      options: {
+        stripBanners: true,
+        sourceMap: true
+      },
+      main: {
+        src: 'js/<%= pkg.name %>.js',
+        dest: 'dist/js/<%= pkg.name %>.js',
+        options: {
+          banner: '<%= banner %>\n' + grunt.file.read('js/umd-intro.js'),
+          footer: grunt.file.read('js/umd-outro.js')
         }
-    });
+      }
+    },
 
-    // These plugins provide necessary tasks.
-    require('load-grunt-tasks')(grunt, {
-        scope: 'devDependencies'
-    });
+    uglify: {
+      options: {
+        banner: '<%= banner %>',
+        output: {
+          ascii_only: true
+        },
+        preserveComments: function (node, comment) {
+          return /^!|@preserve|@license|@cc_on/i.test(comment.value);
+        }
+      },
+      main: {
+        src: '<%= concat.main.dest %>',
+        dest: 'dist/js/<%= pkg.name %>.min.js',
+        options: {
+          sourceMap: true,
+          sourceMapIncludeSources: true,
+          sourceMapIn: 'dist/js/<%= pkg.name %>.js.map'
+        }
+      }
+    },
 
-    // Version numbering task.
-    // to update version number, use grunt version::x.y.z
+    less: {
+      options: {
+        strictMath: true,
+        sourceMap: true,
+        outputSourceFiles: true,
+        sourceMapURL: '<%= pkg.name %>.css.map',
+        sourceMapFilename: '<%= less.css.dest %>.map'
+      },
+      css: {
+        src: 'less/draggable-mcs.less',
+        dest: 'dist/css/<%= pkg.name %>.css'
+      }
+    },
 
-    // CSS distribution
-    grunt.registerTask('build-css', ['clean:css', 'less', 'postcss', 'usebanner:css', 'cssmin']);
+    usebanner: {
+      css: {
+        options: {
+          banner: '<%= banner %>'
+        },
+        src: '<%= less.css.dest %>'
+      }
+    },
 
-    // JS distribution
-    grunt.registerTask('build-js', ['clean:js', 'eslint', 'concat', 'uglify']);
+    copy: {
+      docs: {
+        expand: true,
+        cwd: 'dist/',
+        src: [
+          '**/*'
+        ],
+        dest: 'docs/docs/dist/'
+      }
+    },
 
-    // Copy dist to docs
-    grunt.registerTask('copy-docs', ['clean:docs', 'copy:docs']);
+    cssmin: {
+      options: {
+        compatibility: 'ie8',
+        keepSpecialComments: '*',
+        advanced: false
+      },
+      css: {
+        src: '<%= less.css.dest %>',
+        dest: 'dist/css/<%= pkg.name %>.min.css'
+      }
+    },
 
-    // Build CSS & JS
-    grunt.registerTask('build', ['build-css', 'build-js']);
+    csslint: {
+      options: {
+        'adjoining-classes': false,
+        'box-sizing': false,
+        'box-model': false,
+        'compatible-vendor-prefixes': false,
+        'floats': false,
+        'font-sizes': false,
+        'gradients': false,
+        'important': false,
+        'known-properties': false,
+        'outline-none': false,
+        'qualified-headings': false,
+        'regex-selectors': false,
+        'shorthand': false,
+        'text-indent': false,
+        'unique-headings': false,
+        'universal-selector': false,
+        'unqualified-attributes': false,
+        'overqualified-elements': false
+      },
+      css: {
+        src: '<%= less.css.dest %>'
+      }
+    },
 
-    // Development watch
-    grunt.registerTask('dev-watch', ['build', 'watch']);
+    version: {
+      js: {
+        options: {
+          prefix: 'draggable-mcs.VERSION = \''
+        },
+        src: [
+          'js/<%= pkg.name %>.js'
+        ]
+      },
+      default: {
+        options: {
+          prefix: '[\'"]?version[\'"]?:[ "\']*'
+        },
+        src: [
+          'package.json'
+        ]
+      }
+    },
 
-    // Full distribution
-    grunt.registerTask('dist', ['build', 'compress', 'copy-docs']);
+    postcss: {
+      options: {
+        map: true,
+        processors: [
+          require('autoprefixer')()
+        ]
+      },
+      css: {
+        src: '<%= less.css.dest %>'
+      }
+    },
 
-    // Default task.
-    grunt.registerTask('default', 'build');
+    compress: {
+      zip: {
+        options: {
+          archive: 'draggable-mcs-<%= pkg.version %>.zip',
+          mode: 'zip'
+        },
+        files: [
+          {
+            expand: true,
+            cwd: 'dist/',
+            src: '**',
+            dest: 'draggable-mcs-<%= pkg.version %>/'
+          }, {
+            src: ['package.json'],
+            dest: 'draggable-mcs-<%= pkg.version %>/'
+          }
+        ]
+      }
+    },
 
-    // Linting
-    grunt.registerTask('lint', 'eslint');
+    watch: {
+      gruntfile: {
+        files: '<%= eslint.gruntfile.src %>',
+        tasks: 'eslint:gruntfile'
+      },
+      js: {
+        files: ['<%= eslint.main.src %>'],
+        tasks: 'build-js'
+      },
+      less: {
+        files: 'less/*.less',
+        tasks: 'build-css'
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  require('load-grunt-tasks')(grunt, {
+    scope: 'devDependencies'
+  });
+
+  // Version numbering task.
+  // to update version number, use grunt version::x.y.z
+
+  // CSS distribution
+  grunt.registerTask('build-css', ['clean:css', 'less', 'postcss', 'usebanner:css', 'cssmin']);
+
+  // JS distribution
+  grunt.registerTask('build-js', ['clean:js', 'eslint', 'concat', 'uglify']);
+
+  // Copy dist to docs
+  grunt.registerTask('copy-docs', ['clean:docs', 'copy:docs']);
+
+  // Build CSS & JS
+  grunt.registerTask('build', ['build-css', 'build-js']);
+
+  // Development watch
+  grunt.registerTask('dev-watch', ['build', 'watch']);
+
+  // Full distribution
+  grunt.registerTask('dist', ['build', 'compress', 'copy-docs']);
+
+  // Default task.
+  grunt.registerTask('default', 'build');
+
+  // Linting
+  grunt.registerTask('lint', 'eslint');
 };
