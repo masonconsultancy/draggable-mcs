e36ab521f4ab974780261e6311c0e607ae76d5d1
Gary John Mason <gary.john.mason@gmail.com>
2023-05-24 11:22:48 +0100

    Tidied up the js files and improved the test html file

--- src/draggable-mcs.js
+++ src/draggable-mcs.js
@@ -2,7 +2,7 @@
   'use strict';
 
   $.fn.draggable = function (options) {
-    var defaults = {
+    const defaults = {
       headerIdentifier: '.draggable-header',
       position: {
         dock: null,
@@ -11,14 +11,14 @@
       }
     };
 
-    var settings = $.extend({}, defaults, options);
+    const settings = $.extend({}, defaults, options);
 
     this.filter('div.draggable').each(function () {
-      var item = $(this);
+      const item = $(this);
 
       positionElement(item, settings.position);
 
-      var headerItem = item.find(settings.headerIdentifier) ?? item;
+      const headerItem = item.find(settings.headerIdentifier) ?? item;
 
       headerItem.unbind('mousedown');
       $(document).unbind('mouseup mousemove');
@@ -31,14 +31,14 @@
 
   function positionElement (item, position) {
     if (position.dock) {
-      var dockableItem = $(position.dock);
+      const dockableItem = $(position.dock);
 
       if (!dockableItem) {
         return;
       }
 
-      var y = position.top ?? 0;
-      var x = position.left ?? 0;
+      const y = position.top ?? 0;
+      const x = position.left ?? 0;
 
       item.css({
         top: dockableItem.offset().top + y,
@@ -58,15 +58,13 @@
   }
 
   function dragElement (item, headerItem) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 
     headerItem.on('mousedown', function (mouseDownEvent) {
-      mouseDownEvent = mouseDownEvent || window.event;
       mouseDownEvent.preventDefault();
       pos3 = mouseDownEvent.clientX;
       pos4 = mouseDownEvent.clientY;
       $(document).on('mousemove', function (moveEvent) {
-        moveEvent = moveEvent || window.event;
         moveEvent.preventDefault();
         // calculate the new cursor position:
         pos1 = pos3 - moveEvent.clientX;
